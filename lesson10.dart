1ea2ddab00f6e3b751432f8007999fe1272ce1d4
Akyl <kurmanalievakyl5@gmail.com>
2023-04-28 19:46:54 +0600

    random problems

---
+++ lesson10.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  // int random = Random().nextInt(100);
+  // print(random);
+  // for (int i = 0; i > Random().nextInt(100); i++);
+
+
+  //1
+  // int random1 = Random().nextInt(100);
+  // int random2 = Random().nextInt(100);
+  // int result = random1 + random2;
+  // print("$random1 + $random2 = $result");
+
+  //2
+  // List arr = [];
+  // for (int i = 0; i < Random(arr.length).nextInt(100); i++) {
+  //   arr.add(Random().nextInt(100));
+  // } 
+  // print(arr);
+
+  //3
+  //   List arr1 = List.generate(5, (i) => Random().nextInt(5) +0);
+  //   print(arr1);
+  //   List index = [];
+  //   int? x = int.parse(text("Введите числа в интервале [0,5]"));
+  //   for (int x = 0; x < arr1.length; x++) {
+  //     if (arr1 == x) {
+  //       index.add(x);
+  //     }
+  //   }
+  //   if (index != x) {
+  //     print("No elements $x");
+  //   } else if (x == index) {
+  //     print("$x: $index");
+  //   }
+  // }
+
+  // String text(String text) {
+  //   print(text);
+  //   return stdin.readLineSync()!;
+
+  //4
+  // List array = List.generate(10, (index) => Random().nextInt(51) + 50);
+  // print(array);
+  // bool sameNumbers = false;
+  // for (int i = 0; i < array.length - 1; i++) {
+  //   if (array[i] == array[i + 1]) {  
+  //     sameNumbers = true;
+  //     break;
+  //   }
+  // }
+  // print(array);
+  // if (sameNumbers) {
+  //   print("Одинаковые значения есть");
+  // } else {
+  //   print("Одинаковых значений нет");
+  // }
+}
